ddb2e57ece836fdeaf4def04034108c802147b19
Justin Bardeloza <heartsniper0403@gmail.com>
2021-01-05 05:51:31 +0000

    Exercise8.1

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -9,23 +9,20 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0  
 
 if size == "S":
-  bill = 15
+  bill += 15
 
-  if add_pepperoni == "Y":
-    bill += 2
+elif size == "M":
+  bill += 20
 
-if size == "M":
-  bill = 20
+elif size == "L":
+  bill += 25
 
-  if add_pepperoni == "Y":
-    bill += 3
-
-if size == "L":
-  bill = 25
-
-  if add_pepperoni == "Y":
-    bill += 3
-
+    if add_pepperoni == "Y"
+        if size == "S"
+        bill += 2
+        else
+        bill += 3
+    
 if extra_cheese == "Y":
   bill += 1
 
